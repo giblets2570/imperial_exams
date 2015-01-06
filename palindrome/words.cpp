419f49630d0e130ff606c5406568a6c41825bd28
giblets2570 <tomkeohanemurray@gmail.com>
2015-01-06 18:22:49 +0000

    Done the palindrome question

---
+++ palindrome/words.cpp
@@ -0,0 +1,104 @@
+#include <iostream>
+#include <cstring>
+#include <cctype>
+
+#include "words.h"
+
+using namespace std;
+
+void reverse(const char* word, char* reversed_word){
+  int index = 0;
+  for(int i = strlen(word) - 1; i >=0; i--){
+    reversed_word[index++] = word[i];
+  }
+  reversed_word[index] = '\0';
+  return;
+}
+
+bool compare(const char* one, const char* two){
+  if(*one == '\0'){
+    if(*two == '\0')
+      return true;
+    else if(isalnum(*two)){
+      return false;
+    }
+    else
+      return compare(one, ++two);
+  }
+  if(*two == '\0'){
+    if(isalnum(*one)){
+      return false;
+    }
+    else
+      return compare(++one, two);
+  }
+
+  if(!isalnum(*one))
+    return compare(++one, two);
+  if(!isalnum(*two))
+    return compare(one, ++two);    
+  if(tolower(*one) != tolower(*two))
+    return false;
+
+  return compare(++one,++two);
+}
+
+
+bool palindrome(const char* sentence){
+  char reversed_sentence[512];
+  reverse(sentence, reversed_sentence);
+  return compare(sentence, reversed_sentence);
+}
+
+bool anagram(const char* str1,const char* str2){
+  char sorted_str1[512], sorted_str2[512];
+  lower_sorted_letters(str1,sorted_str1);
+  lower_sorted_letters(str2,sorted_str2);
+
+  int length1 = strlen(sorted_str1);
+  int length2 = strlen(sorted_str2);
+
+  if(length1 != length2)
+    return false;
+
+  int i = 0;
+
+  while(sorted_str1[i] != '\0'){
+    if(sorted_str1[i] != sorted_str2[i])
+      return false;
+    i++;
+  }
+
+  return true;
+
+}
+
+void lower_sorted_letters(const char* sentence, char* sorted_letters){
+  int i = 0, j = 0;
+  while(sentence[i] != '\0'){
+    if(isalpha(sentence[i]))
+      sorted_letters[j++] = tolower(sentence[i]);
+    i++;
+  }
+
+  int length = j;
+
+  for(i = 0; i < length - 1; i++){
+    int lowest = i;
+    for(j = i + 1; j < length; j++){
+      if(sorted_letters[j] < sorted_letters[lowest])
+        lowest = j;
+    }
+    swap(sorted_letters[i],sorted_letters[lowest]);
+  }
+
+  sorted_letters[length] = '\0';
+
+  return;
+}
+
+void swap(char& ch1, char& ch2){
+  char temp = ch1;
+  ch1 = ch2;
+  ch2 = temp;
+}
---
+++ palindrome/words.h
@@ -0,0 +1,16 @@
+#ifndef WORDS_H
+#define WORDS_H
+
+void reverse(const char* word, char* reversed_word);
+
+bool compare(const char* one, const char* two);
+
+bool palindrome(const char* sentence);
+
+bool anagram(const char* str1, const char* str2);
+
+void lower_sorted_letters(const char* sentence, char* sorted_letters);
+
+void swap(char& ch1, char& ch2);
+
+#endif
